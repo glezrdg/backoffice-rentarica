ed7653e73fa82c5eb8d3ccea8a86a4e2d404a8d3
GermanGlezRguez <glezrdg@gmail.com>
2024-11-05 20:44:51 -0400

    Checking in

--- src/components/shared/Button/Button.tsx
+++ src/components/shared/Button/Button.tsx
@@ -1,27 +1,27 @@
-import React from 'react'
+import React from "react";
 
 // css
-import './styles.css'
+import "./styles.css";
 
 interface IButtonProps {
-  children?: React.ReactNode
-  className?: string
-  color?: 'primary' | 'success' | 'warning' | 'info' | 'danger' | 'white'
-  type?: 'fill' | 'outline'
-  buttonType?: 'submit' | 'button'
-  rounded?: boolean
-  text?: string
-  tooltip?: string
-  icon?: any
-  iconButton?: boolean
-  disabled?: boolean
-  onClick?: (e: any) => void
+  children?: React.ReactNode;
+  className?: string;
+  color?: "primary" | "success" | "warning" | "info" | "danger" | "white";
+  type?: "fill" | "outline";
+  buttonType?: "submit" | "button";
+  rounded?: boolean;
+  text?: string;
+  tooltip?: string;
+  icon?: any;
+  iconButton?: boolean;
+  disabled?: boolean;
+  onClick?: (e: any) => void;
 }
 
 const Button: React.FC<IButtonProps> = ({
-  color = 'primary',
+  color = "primary",
   disabled = false,
-  type = 'fill',
+  type = "fill",
   className,
   rounded,
   icon,
@@ -33,19 +33,19 @@ const Button: React.FC<IButtonProps> = ({
 }) => {
   // Function that returns a class dependind on props
   const showClases = () => {
-    let classText = ''
+    let classText = "";
 
-    if (iconButton) classText += ' p-2'
-    if (rounded || iconButton) classText += ' rounded-full'
+    if (iconButton) classText += " p-2";
+    if (rounded || iconButton) classText += " rounded-full";
 
-    return classText
-  }
+    return classText;
+  };
 
   return (
     <button
       data-te-ripple-init
-      data-te-ripple-color={type === 'fill' ? 'light' : 'dark'}
-      className={`group btn ${type} ${color} ${className} ${showClases()}`}
+      data-te-ripple-color={type === "fill" ? "light" : "dark"}
+      className={`text-xl group btn ${type} ${color} ${className} ${showClases()}`}
       onClick={(e) => onClick && onClick(e)}
       type={buttonType}
       disabled={disabled}
@@ -53,17 +53,17 @@ const Button: React.FC<IButtonProps> = ({
       {icon && icon}
       {text}
       {tooltip && (
-        <div className='group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-8 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100'>
-          <div className='flex max-w-xs flex-col items-center'>
-            <div className='rounded bg-gray-900 p-2 text-xs text-center shadow-lg'>
+        <div className="group-hover:[transform:perspective(0px)_translateZ(0)_rotateX(0deg)] absolute bottom-8 mb-6 origin-bottom transform rounded text-white opacity-0 transition-all duration-300 group-hover:opacity-100">
+          <div className="flex max-w-xs flex-col items-center">
+            <div className="rounded bg-gray-900 p-2 text-xs text-center shadow-lg">
               {tooltip}
             </div>
-            <div className='clip-bottom h-2 w-4 bg-gray-900'></div>
+            <div className="clip-bottom h-2 w-4 bg-gray-900"></div>
           </div>
         </div>
       )}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
--- src/pages/Properties/Properties.tsx
+++ src/pages/Properties/Properties.tsx
@@ -1,37 +1,40 @@
-import { InputText } from 'primereact/inputtext'
-import React, { useState } from 'react'
-import { Button } from '../../components/shared'
-import CreatePropertyModal from './components/modals/CreatePropertyModal'
-import ProjectCard from './components/ProjectCard'
-import { usePropertyState } from './context'
+import { InputText } from "primereact/inputtext";
+import React, { useState } from "react";
+import { Button } from "../../components/shared";
+import CreatePropertyModal from "./components/modals/CreatePropertyModal";
+import ProjectCard from "./components/ProjectCard";
+import { usePropertyState } from "./context";
 
 interface IPropertiesProps {
-  children?: React.ReactNode
+  children?: React.ReactNode;
 }
 
 const Properties: React.FC<IPropertiesProps> = (props) => {
-  const { properties } = usePropertyState()
-  const [create, setCreate] = useState(false)
+  const { properties } = usePropertyState();
+  const [create, setCreate] = useState(false);
 
   return (
     <>
-      <div className='mb-6 flex justify-between items-center'>
-        <div>
-          <InputText
-            className=' w-[60vw]lg:w-[30vw]'
-            placeholder='Buscar por nombre o codigo'
-          />
-        </div>
-        <Button onClick={() => setCreate(true)} text='Crear' />
+      <div className="mb-6 flex justify-between items-center">
+        <InputText
+          className=" w-[50vw] px-10"
+          placeholder="Buscar por nombre o codigo"
+        />
+
+        <Button
+          className="text-xl px-4"
+          onClick={() => setCreate(true)}
+          text="Crear"
+        />
       </div>
-      <div className='grid md:grid-cols-2 lg:grid-cols-3 gap-8'>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
         {properties?.map((p) => (
           <ProjectCard project={p} />
         ))}
       </div>
       <CreatePropertyModal visible={create} onHide={() => setCreate(false)} />
     </>
-  )
-}
+  );
+};
 
-export default Properties
+export default Properties;
