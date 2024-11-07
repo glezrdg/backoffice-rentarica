9e92f593e51522f40ec794b931cbc319af94e096
GermanGlezRguez <glezrdg@gmail.com>
2024-11-06 21:12:57 -0400

    merded

--- src/components/shared/UploadFile.tsx
+++ src/components/shared/UploadFile.tsx
@@ -1,5 +1,5 @@
-import { API_URL } from '../../utility/constants'
-import { useEffect, useState } from 'react'
+import { API_URL } from "../../utility/constants";
+import { useEffect, useState } from "react";
 
 export default function UploadFile({
   setFiles,
@@ -13,70 +13,70 @@ export default function UploadFile({
 
   useEffect(() => {
     if (images.length) {
-      const imgInputHelper: any = document.getElementById(imageId)!
-      const imgInputHelperLabel = document.getElementById(labelId)!
-      const imgContainer = document.querySelector(`.${containerClass}`)!
+      const imgInputHelper: any = document.getElementById(imageId)!;
+      const imgInputHelperLabel = document.getElementById(labelId)!;
+      const imgContainer = document.querySelector(`.${containerClass}`)!;
 
       images.map((i: string) => {
-        const newImg = document.createElement('img')
-        newImg.src = `${API_URL}uploads/${i}`
-        imgContainer.insertBefore(newImg, imgInputHelperLabel)
-      })
+        const newImg = document.createElement("img");
+        newImg.src = `${API_URL}uploads/${i}`;
+        imgContainer.insertBefore(newImg, imgInputHelperLabel);
+      });
     }
-  }, [images])
+  }, [images]);
 
   const addImgHandler = () => {
-    const imgInputHelper: any = document.getElementById(imageId)!
-    const imgInputHelperLabel = document.getElementById(labelId)!
-    const imgContainer = document.querySelector(`.${containerClass}`)!
-    console.log(imgContainer)
+    const imgInputHelper: any = document.getElementById(imageId)!;
+    const imgInputHelperLabel = document.getElementById(labelId)!;
+    const imgContainer = document.querySelector(`.${containerClass}`)!;
+    console.log(imgContainer);
 
-    const IMG_FILES: File[] = []
+    const IMG_FILES: File[] = [];
 
-    const fileInput = imgInputHelper.files[0]
-    if (!fileInput) return
+    const fileInput = imgInputHelper.files[0];
+    if (!fileInput) return;
     // Generate img preview
-    const reader = new FileReader()
-    reader.readAsDataURL(fileInput)
+    const reader = new FileReader();
+    reader.readAsDataURL(fileInput);
     reader.onload = () => {
-      const newImg = document.createElement('img')
-      newImg.src = String(reader.result)
-      imgContainer.insertBefore(newImg, imgInputHelperLabel)
-    }
+      const newImg = document.createElement("img");
+      newImg.src = String(reader.result);
+      imgContainer.insertBefore(newImg, imgInputHelperLabel);
+    };
     // Store img file
-    IMG_FILES.push(fileInput)
-    imgFiles((prev: any) => [...prev, fileInput])
-    setFiles(IMG_FILES[0])
+    IMG_FILES.push(fileInput);
+    imgFiles((prev: any) => [...prev, fileInput]);
+    setFiles(IMG_FILES[0]);
     // Reset image input
-    imgInputHelper.value = ''
-    console.log('IMG FILE', IMG_FILES)
-    return
-  }
+    imgInputHelper.value = "";
+    console.log("IMG FILE", IMG_FILES);
+    return;
+  };
 
   return (
-    <div className='flex flex-col my-5'>
-      <div className='custom__form'>
+    <div className="flex flex-col my-5">
+      <div className="custom__form">
         <div className={containerClass}>
           <label id={labelId} htmlFor={imageId}>
             +
           </label>
           <input
-            type='file'
+            type="file"
             id={imageId}
-            name='images'
-            accept='image/jpeg image/png image/webp'
+            name="images"
+            accept="image/jpeg image/png image/webp"
             onChange={addImgHandler}
           />
         </div>
         <input
-          type='file'
-          id='image-input'
-          name='images'
-          accept='image/jpeg image/png image/webp'
+          type="file"
+          id="image-input"
+          name="images"
+          accept="image/jpeg image/png image/webp"
           multiple
         />
         <br />
       </div>
     </div>
-  )
+  );
 }
